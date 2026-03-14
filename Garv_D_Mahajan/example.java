eae8928f7e4c82cc2b794f20537c6d111abd332d
garvmahajan20 <garvmahajan20107@gmail.com>
2026-03-14 17:01:27 +0530

    Add example  program for basic calculation
    Display output to the terminal

---
+++ Garv_D_Mahajan/example.java
@@ -0,0 +1,7 @@
+public class example{
+  public static void main(String[] args){
+    int a=5,b=6;
+    System.out.println("The addition of 2 numbers a and b is"+" "+(a+b));
+    System.out.println("The difference of 2 numbers a and b is"+" "+(a-b));
+  }
+}
